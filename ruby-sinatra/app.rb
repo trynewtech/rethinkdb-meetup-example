c89c961f75677c56943aea9781657845f5b2f7f5
Steve McCarthy <steve@redlua.com>
2014-02-03 20:17:39 -0500

    Add sinatra based ruby tutorial

---
+++ ruby-sinatra/app.rb
@@ -0,0 +1,61 @@
+require 'sinatra/base'
+require 'slim'
+
+class Rethink < Sinatra::Base
+
+  get '/' do slim :index end
+  get '/post' do slim :makepost end
+
+  #
+  # DO THINGS HERE
+  #
+
+  post '/post' do
+    postInfo = params[:post]
+    #TODO: save the post
+    post = {}
+    redirect '/post' + post.id
+  end
+
+  get '/post/:id' do
+    postId = params[:id]
+    #TODO: get the post by it's id (use postId)
+    @post = {}
+    #Optional TODO: get the users information
+    @user = {}
+    slim :post
+  end
+
+  get '/users' do
+    #TODO: get all users
+    @users = []
+    slim :users
+  end
+
+  get '/posts' do
+    #TODO: get all posts
+    @posts = []
+    slim :posts
+  end
+
+  get '/user/:id/posts' do
+    userId = params[:id]
+    #TODO: get all postts by a user (user their userId)
+    @posts = []
+    slim :posts
+  end
+
+  # ADVANCED
+  # get post by title
+  get '/post/title/:title' do
+    postTitle = params[:title]
+    #TODO: get the post by it's title
+    @post = {}
+    slim :post
+  end
+
+  #
+  # OK STOP DOING THINGS
+  #
+
+end
