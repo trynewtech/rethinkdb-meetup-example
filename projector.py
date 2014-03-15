551da5801775e78877ea089bd645c5d2da1c9f18
Joe Esposito <joe@joeyespo.com>
2014-03-15 01:38:02 -0400

    Initial projector view.

---
+++ projector.py
@@ -0,0 +1,14 @@
+import rethinkdb as r
+from flask import Flask, request, render_template, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
