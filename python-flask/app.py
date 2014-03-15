e10dfe21d11122ba8899e0092cb3eaa0f627051f
Joe Esposito <joe@joeyespo.com>
2014-03-14 21:53:26 -0400

    PEP8, move helpers down.

--- python-flask/app.py
+++ python-flask/app.py
@@ -1,27 +1,23 @@
 from flask import Flask, render_template, request
 import rethinkdb as r
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-@app.route('/post')
-def make_post():
-    return render_template('makepost.html')
 
 '''
 ### DO THINGS HERE
 '''
 
-#TODO: #1 Look at the README to add yourself to the blog
+# TODO: #1 Look at the README to add yourself to the blog
 
-#TODO: #2 make database connection
+# TODO: #2 Make database connection
 connection = r.connect()
 
 
 @app.route('/users')
 def users():
-    #TODO: #3 get all users
+    # TODO: #3 Get all users
     users = []
 
     return render_template('users.html', users=users)
@@ -32,7 +28,7 @@ def post_post():
     post_title = request.form['title']
     post_text = request.form['text']
 
-    #TODO: #4 save the post
+    # TODO: #4 Save the post to the database
     post = {}
 
     return redirect(url_for('post', id=post['id']))
@@ -40,7 +36,7 @@ def post_post():
 
 @app.route('/post/<id>')
 def post(id):
-    #TODO: #5 get the post by its id (use id)
+    # TODO: #5 Get the post by its id (use id)
     post = {}
 
     return render_template('post.html', post=post)
@@ -48,7 +44,7 @@ def post(id):
 
 @app.route('/posts')
 def posts():
-    #TODO: #6 get all posts
+    # TODO: #6 Get all posts
     posts = []
 
     return render_template('posts.html', posts=posts)
@@ -56,29 +52,32 @@ def posts():
 
 @app.route('/user/<id>/posts')
 def user_posts(id):
-    #TODO: #7 get all posts by a user (use their id)
+    # TODO: #7 Get all posts by a user (use their id)
     posts = []
 
     return render_template('posts.html', posts=posts)
 
 
-# ADVANCED
-# get post by title
-@app.route('/post/title/<title>')
+# --- ADVANCED ---
+
+@app.route('/post/<title>')
 def post_by_title(title):
-    #TODO: #8 get the post by its title
+    """Displays the post given its title."""
+
+    # TODO: #8 Get the post by its title
     post = {}
 
     return render_template('post.html', post=post)
 
 
-# get post and author (user) information
 @app.route('/post/<id>/details')
 def post_details(id):
-    #TODO: #9 get the post by its id (use id) (you did this before :)
+    """Displays the post and additional details, i.e. the author information."""
+
+    # TODO: #9 Get the post by its id (use id) (you did this before :)
     post = {}
 
-    #TODO: #10 get the user's information
+    # TODO: #10 Get the user's information
     user = {}
 
     return render_template('post.html', post=post, user=user)
@@ -87,6 +86,15 @@ def post_details(id):
 '''
 ### OKAY STOP DOING THINGS
 '''
+
+
+# Views you don't care about
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/post')
+def make_post():
+    return render_template('makepost.html')
 
 
 if __name__ == "__main__":
