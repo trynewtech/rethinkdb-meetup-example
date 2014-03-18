4ce48c87112c07823743d80434368ea3e1eff218
Steve McCarthy <steve@redlua.com>
2014-03-17 22:52:56 -0400

    Update Ruby example to match other examples
    Includes various ruby syntax fixes.

--- ruby-sinatra/app.rb
+++ ruby-sinatra/app.rb
@@ -11,82 +11,127 @@ class Rethink < Sinatra::Base
   get '/post' do slim :makepost end
 
   #
-  # DO THINGS HERE
+  # COMPLETE THE FOLLOWING TODOS
   #
   
-  #TODO: #1 look at the README to add yourself to the blog
+  # TODO: #1. Look at the README to add yourself to the blog
 
-  #TODO: #2 make database connection
-  connection = RethinkDB::Connection.new()
+  # TODO: #2. Make database connection
+  # Hint: Set the following variables to actual values provided by the organizers
+=begin
+  host = ''
+  port = 0
+  auth_key = ''
+  db = ''
+
+  connection = r.connect(:host => host,
+                         :port => port,
+                         :db => db,
+                         :auth_key => auth_key)
+=end
+
+  # TODO: #3. Test it by running this file and visiting http://localhost:4567
 
   get '/users' do
-    #TODO: #3 get all users
+    # TODO: #4. Get all users
+    # Hint: The RethinkDB query will return an interable object
     @users = []
 
     slim :users
   end
 
+  # TODO: #5. Visit http:localhost:4567/users in the browser to test #4
+
   post '/post' do
-    post_info = params[:post]
+    post = {
+      :title => params[:title],
+      :slug => slugify(params[:title]),
+      :text => params[:text]
+    }
 
-    #TODO: #4 save the post
-    post = {}
+    # TODO: #6. Save the post to the database and get the resulting ID
+    # Hint: Use "post_info" in your query
+    # Hint: Get the ID from the result of the query
+    post_id = ''
 
-    redirect '/post' + post.id
+    redirect '/post' + post_id
   end
 
   get '/post/:id' do
     post_id = params[:id]
 
-    #TODO: #5 get the post by its id (use postId)
+    # TODO: #7. Get the post by its id (use post_id)
     @post = {}
 
     slim :post
   end
 
+  # TODO: #8. Visit http://localhost:4567/post and create a few posts
 
   get '/posts' do
-    #TODO: #6 get all posts
+    # TODO: #9. Get all posts
     @posts = []
 
     slim :posts
   end
+
+  # TODO: #10. Visit http:localhost:4567/posts to see everyone's posts so far
 
   get '/user/:id/posts' do
     user_id = params[:id]
 
-    #TODO: #7 get all posts by a user (user their userId)
+    # TODO: #11. Get all posts by a user using the provided "user_id" variable
     @posts = []
 
     slim :posts
   end
 
-  # ADVANCED
-  # get post by title
-  get '/post/title/:title' do
-    post_title = params[:title]
+  #
+  # CONGRATULATIONS
+  # You've completed the beginner challenges.
+  # Now see if you can complete the following!
+  #
+  
+  # TODO: #13. Visit the Rethinkdb interface again to see how the data is being
+  # stored. Note: this is a shared database, please respect each others' data.
 
-    #TODO: #8 get the post by its title
+
+  # Displays the post given it's title slug
+  get '/post/title/:slug' do
+    post_slug = params[:slug]
+
+    # TODO: #14. Get a post by its slug by using the "post_slug" variable
     @post = {}
 
     slim :post
   end
 
-  # get post and author (user) information
+  # TODO: #15. Visit a page and click on the "friendly URL" link to test it
+
+  # Displays the post and additional details, i.e. the author information
   get '/post/:id/details' do
     post_id = params[:id]
 
-    #TODO: #9 get the post by its id (use postId) (you did this before :)
+    # TODO: #16. Get the post by its id (use postId), and get the authors information
+    # Hint: Use a table join
     @post = {}
-
-    #TODO: #10 get the users information
     @user = {}
 
     slim :post
   end
 
-  #
-  # OK STOP DOING THINGS
-  #
+  # TODO: #17. Go back to a post page and visit the "Post details" at the bottom
 
+  #
+  # CONGRATULATIONS
+  # You've completed the intermediate challenges.
+  # Take a breather. Talk to a nearby mentor when you're ready for more.
+  #
+  
+  # Helpers
+  def slugify(title)
+    title.gsub(' ', '-').downcase
+  end
+
+  run! if app_file == $0
 end
