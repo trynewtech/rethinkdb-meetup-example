c519a60dcd19f6ec5e7fc849fe9a8134d6b6b292
Joe Esposito <joe@joeyespo.com>
2014-03-19 18:14:24 -0400

    Add projector.

--- projector.py
+++ projector.py
@@ -1,5 +1,6 @@
 import rethinkdb as r
-from flask import Flask, request, render_template, redirect, url_for
+from werkzeug.local import LocalProxy
+from flask import Flask, g, request, render_template, redirect, url_for
 
 
 # Flask application
@@ -7,11 +8,53 @@ app = Flask(__name__)
 app.config.from_object('settings')
 app.config.from_envvar('SETTINGS_MODULE', silent=True)
 app.config.from_pyfile('settings_local.py', silent=True)
+# Database
+def get_db():
+    if not hasattr(g, 'db'):
+        g.db = r.connect(
+            host=app.config['DATABASE_HOST'],
+            port=app.config['DATABASE_PORT'],
+            db=app.config['DATABASE_DB'],
+            auth_key=app.config['DATABASE_AUTH_KEY'],
+        )
+    return g.db
+db = LocalProxy(get_db)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/user-list')
+def user_list():
+    #users = [
+    #    {
+    #        'name': 'One',
+    #    },
+    #]
+    cursor = r.table('users').run(db)
+    users = list(cursor)
+    return render_template('user-list.html', users=users)
+
+
+@app.route('/post-list')
+def post_list():
+    #posts = [
+    #    {
+    #        'title': 'Title!',
+    #    }
+    #]
+    #cursor = r.table('posts')\
+    #    .eq_join('user_id', r.table('users'))\
+    #    .filter(r.row['left']['id'] == id)\
+    #    .run(connection)
+    #post_and_user = list(cursor)
+    #post = post_and_user[0]['left']
+    #user = post_and_user[0]['right']
+    cursor = r.table('posts').run(db)
+    posts = list(cursor)
+    return render_template('post-list.html', posts=posts)
 
 
 # Run development server
--- settings.py
+++ settings.py
@@ -8,8 +8,16 @@ Use the appropriate environment variable or create a settings_local.py instead.
 import os
 
 
+# Server settings
 HOST = os.environ.get('HOST')
 PORT = os.environ.get('PORT')
 DEBUG = os.environ.get('DEBUG')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SERVER_NAME = os.environ.get('SERVER_NAME')
+
+
+# Database settings
+DATABASE_HOST = os.environ.get('DATABASE_HOST')
+DATABASE_PORT = os.environ.get('DATABASE_PORT')
+DATABASE_DB = os.environ.get('DATABASE_DB')
+DATABASE_AUTH_KEY = os.environ.get('DATABASE_AUTH_KEY')
